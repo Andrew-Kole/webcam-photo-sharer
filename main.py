db9047d93dbd1e2ec21bf649946482bd419c8487
pakitowka <whitesaint12@gmail.com>
2023-07-30 20:24:53 +0200

    cosmetic changes

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ class CameraScreen(Screen):
 
     def start(self):
         """Starts camera and displays it in screen."""
+        self.ids.camera.opacity = 1
         self.ids.camera.play = True
         self.ids.camera_switch.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
         """Stops camera and cleans the screen."""
+        self.ids.camera.opacity = 0
         self.ids.camera.play = False
         self.ids.camera_switch.text = 'Start Camera'
         self.ids.camera.texture = None
