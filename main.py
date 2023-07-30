0a6afd3cf8ae3ee1091343ae37ec5d28dc6d9513
pakitowka <whitesaint12@gmail.com>
2023-07-30 17:17:29 +0200

    implementing copy link and open it in browser

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 import time
+import webbrowser
 from filesharer import FileSharer
 
 
@@ -11,13 +13,13 @@ Builder.load_file('frontend.kv')
 class CameraScreen(Screen):
 
     def start(self):
-        """Starts camera and displays it in screen"""
+        """Starts camera and displays it in screen."""
         self.ids.camera.play = True
         self.ids.camera_switch.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        """Stops camera and cleans the screen"""
+        """Stops camera and cleans the screen."""
         self.ids.camera.play = False
         self.ids.camera_switch.text = 'Start Camera'
         self.ids.camera.texture = None
@@ -26,7 +28,7 @@ class CameraScreen(Screen):
         """Captures a frame from camera, saves an image
         with current date and time as name in images directory
         and switches to another screen, where image is displayed
-        and open access to image sharing functionality"""
+        and open access to image sharing functionality."""
         current_time = time.strftime('%Y%m%d-%H%M%S')
         self.filepath = f'images/{current_time}.png'
         self.ids.camera.export_to_png(self.filepath)
@@ -36,12 +38,27 @@ class CameraScreen(Screen):
 
 class ImageScreen(Screen):
 
+    link_message = "Create a Link First"
     def create_link(self):
+        """Shares a frame in cloud and displays link in the label."""
         filepath = App.get_running_app().root.ids.camera_screen.filepath
         filesharer = FileSharer(filepath=filepath)
-        url = filesharer.share()
-        self.ids.link.text = url
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
 
+    def copy_link(self):
+        """Copies link."""
+        try:
+            Clipboard.copy(self.url)
+        except AttributeError:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        """Opens link in a browser."""
+        try:
+            webbrowser.open(self.url)
+        except AttributeError:
+            self.ids.link.text = self.link_message
 
 class RootWidget(ScreenManager):
     pass
